15b3b1cb0664a9c34f64e083ee2fae53351bbdcb
FiveMinCode <fivemincode@gmail.com>
2021-08-01 20:22:56 +0530

    Intial commit

---
+++ REST/REST/Controllers/BooksController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using REST.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public BooksController()
+        {
+            
+        }
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            return Ok(new List<Book>() { new Book()
+            {
+                Id = 1,
+                Genre = "Computers",
+                ReleaseDate = DateTime.Now.AddDays(-1500)
+            } });
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetBook(int id)
+        {
+            return Ok(new Book()
+            {
+                Id = 1,
+                Genre = "Computers",
+                ReleaseDate = DateTime.Now.AddDays(-1500)
+            });
+        }
+        [HttpPost]
+        public IActionResult SaveBook(Book book)
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateBook(Book book)
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteBook(int id)
+        {
+            return Ok();
+        }
+    }
+}
